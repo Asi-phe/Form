44fb5a1d458bb184c9ca7fa856eb8c1373256982
asiphe-m <42046397+Asi-phe@users.noreply.github.com>
2021-02-22 10:39:03 +0200

    Add files via upload
    I have created a form using JavaScript CSS3 and HTML5. the form is for users to be able to validate their details.

---
+++ form.js
@@ -0,0 +1,39 @@
+function registration(){
+var name = document.forms["RegForm"]["name"];
+var Second name = document.forms["RegForm"]["name"];
+var name = document.forms["RegForm"]["name"];
+var name = document.forms["RegForm"]["name"];
+var name = document.forms["RegForm"]["name"];
+var name = document.forms["RegForm"]["name"];
+}
+           if(name.value == ""){
+             window.alert("Please Enter Your Name")
+             name.focus();
+             return false
+           }
+           if(second name.value == ""){
+             window.alert("Please Enter Your Name")
+             name.focus();
+             return false
+           }
+            if(email.value == ""){
+               window.alert("Please Enter Your Name")
+               name.focus();
+               return false
+            }
+            if(password.value == ""){
+               window.alert("Please Enter Your Name")
+               name.focus();
+               return false
+            }
+            if(mobile.value == ""){
+              window.alert("Please Enter Your Name")
+              name.focus();
+              return false
+            }
+            if(sub.electedIndex <1){
+              window.alert("Please Enter Your Subject")
+              Subject.focus();
+              return false
+            }
+            return true
